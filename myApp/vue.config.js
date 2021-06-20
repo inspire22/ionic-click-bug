d1d91309cb8a9d83607f8c7fa7a7a106ab34bf6f
Kevin VSCode <kevin@allpoetry.com>
2021-06-20 09:56:40 -0700

    adding isCustomElement ion- makes nothing render

--- myApp/vue.config.js
+++ myApp/vue.config.js
@@ -2,4 +2,22 @@ module.exports = {
 	configureWebpack: {
 		devtool: "source-map",
 	},
+
+	chainWebpack: (config) => {
+		config.resolve.alias.set("vue", "@vue/compat");
+
+		config.module
+			.rule("vue")
+			.use("vue-loader")
+			// .loader(require.resolve("vue-loader-v16"))
+			.tap((options) => {
+				return {
+					...options,
+					compilerOptions: {
+						...(options.compilerOptions || {}),
+						isCustomElement: (tag) => /^(trix|gcse|ion)/i.test(tag),
+					},
+				};
+			});
+	},
 };
